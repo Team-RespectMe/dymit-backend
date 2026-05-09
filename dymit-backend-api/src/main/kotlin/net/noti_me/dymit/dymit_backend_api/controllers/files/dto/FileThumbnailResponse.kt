3bda2e02b36cdfd4ed07303a3761eef28435a447
dohoon <elensar92@gmail.com>
2026-05-09 19:22:28 +0900

    v0.12.1 Response 객체 BaseResponse 미상속 부분 수정

--- dymit-backend-api/src/main/kotlin/net/noti_me/dymit/dymit_backend_api/controllers/files/dto/FileThumbnailResponse.kt
+++ dymit-backend-api/src/main/kotlin/net/noti_me/dymit/dymit_backend_api/controllers/files/dto/FileThumbnailResponse.kt
@@ -2,6 +2,7 @@ package net.noti_me.dymit.dymit_backend_api.controllers.files.dto
 
 import io.swagger.v3.oas.annotations.media.Schema
 import net.noti_me.dymit.dymit_backend_api.application.file.dto.FileThumbnailDto
+import net.noti_me.dymit.dymit_backend_api.common.response.BaseResponse
 
 /**
  * 파일 썸네일 응답 DTO입니다.
@@ -9,12 +10,12 @@ import net.noti_me.dymit.dymit_backend_api.application.file.dto.FileThumbnailDto
  * @param path 썸네일 상대 경로
  * @param url 썸네일 접근 URL
  */
-data class FileThumbnailResponse(
+class FileThumbnailResponse(
     @Schema(description = "썸네일 상대 경로", example = "/dymit/thumbnails/A/B/UUID_2026_04_27_21_30_10_thumbnail.jpg")
     val path: String,
     @Schema(description = "썸네일 접근 URL", example = "https://cdn.example.com/dymit/thumbnails/A/B/UUID_2026_04_27_21_30_10_thumbnail.jpg")
     val url: String
-) {
+): BaseResponse() {
 
     companion object {
 
